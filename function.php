f370801c7ca8d301ec5f696c0ff39785d9543d2b
sn9465604 <48965087+sn9465604@users.noreply.github.com>
2020-05-27 14:04:18 +0300

    Update function.php

--- function.php
+++ function.php
@@ -1,26 +1,21 @@
-jQuery(document).ready(function($){
-		$('.woocommerce-cart-form').submit(function(e){
-			e.preventDefault();
-			var form_data = $(this).serialize();
-			var quantity1 = $('.qty').eq(0).val();
-			var quantity2 = $('.qty').eq(1).val();
-			var quantity3 = $('.qty').eq(2).val();
-			var s_data = {
-				'action': 'add_cart_products',
-				'quantity_1':  quantity1,
-				'quantity_2':  quantity2,
-				'quantity_3':  quantity3
-			};
-			$.ajax({
-			  url: "/wp-admin/admin-ajax.php",
-			  method: "POST",
-			  data: s_data,
-			  beforeSend: function(){
+//Add to Cart products fro dashboard
+add_action('wp_ajax_add_cart_products', 'add_cart_products');
+add_action('wp_ajax_nopriv_add_cart_products', 'add_cart_products');
 
-			  },
-			  success: function(response){
-			  	setTimeout(function(){ location.reload(true); }, 1000);
-			  }
-			});
-		});
-	});
+function add_cart_products(){
+	$product_ids = [15,14,13];
+
+	foreach ($product_ids as $key => $value) {
+		$p_hname = 'quantity_'.($key + 1);
+		$p_hcount = $_POST[$p_hname];
+		$product_cart_id = WC()->cart->generate_cart_id( $value );
+		$cart_item_key = WC()->cart->find_product_in_cart( $product_cart_id );
+		if ( $cart_item_key ){
+			WC()->cart->remove_cart_item( $cart_item_key );
+		}
+		WC()->cart->add_to_cart( $value, $p_hcount );
+
+	}
+
+	return json_encode('success');
+}
